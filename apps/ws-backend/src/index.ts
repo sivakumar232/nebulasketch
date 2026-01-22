17e04c43fb1603b0d501c43664ab4b0349f194a7
Sivakumar <sivakumarr3105@gmail.com>
2026-01-22 23:52:35 +0530

    fix:return response after creation

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,9 +1,21 @@
 import { WebSocketServer } from "ws";
-import jwt from "jsonwebtoken"
+import jwt, { decode } from "jsonwebtoken"
 import { JWT_SECRET } from "@repo/backend-common/config";
 
 
 const wss= new WebSocketServer({port:8080}) 
+
+function checkuser(token:string):string| null{
+    const decoded =jwt.verify(token,JWT_SECRET)
+    if(typeof decoded=="string"){
+        return  null;
+    }
+    if(!decoded || decoded.userId){
+        return null;
+    }
+
+    return decoded.userId;
+}
 
 wss.on('connection',function(ws,request){
 
@@ -13,12 +25,12 @@ wss.on('connection',function(ws,request){
     }
     const queryParams = new URLSearchParams(url.split('?')[1]);
     const token =queryParams.get('token') || "";
-    const decoded =jwt.verify(token,JWT_SECRET)
-    if(typeof decoded=="string"){
+    const userId=checkuser(token)
+    if(!userId){
         ws.close();
-        return ;
     }
-    if(!decoded || decoded.userId){
 
-    }
+    ws.on("message",function message(data){
+        ws.send('pong')
+    })
 })
